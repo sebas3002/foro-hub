7f61213f06ae0e6ea707c6cc5c85209fc62451b3
sebas3002 <vsebas3002@gmail.com>
2026-02-18 23:40:36 -0600

    Diagrama de base de datos parte 1

--- src/main/java/com/aluracursos/forohub/ForoHubApplication.java
+++ src/main/java/com/aluracursos/forohub/ForoHubApplication.java
@@ -1,4 +1,4 @@
-package com.aluracursos.demo;
+package com.aluracursos.forohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
