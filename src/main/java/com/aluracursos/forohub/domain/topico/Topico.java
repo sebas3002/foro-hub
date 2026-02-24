69d9cdef7d564322d5af4a98fac55665000fc708
sebas3002 <vsebas3002@gmail.com>
2026-02-23 23:06:28 -0600

    Parte 4 Construccion de la base de datos

---
+++ src/main/java/com/aluracursos/forohub/domain/topico/Topico.java
@@ -0,0 +1,28 @@
+package com.aluracursos.forohub.domain.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+    private String status;
+    private String autor;
+    private String curso;
+
+}
---
+++ src/main/java/com/aluracursos/forohub/repository/TopicoRepository.java
@@ -0,0 +1,7 @@
+package com.aluracursos.forohub.repository;
+
+import com.aluracursos.forohub.domain.topico.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
